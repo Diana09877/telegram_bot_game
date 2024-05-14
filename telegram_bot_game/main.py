56bfc8e04125c32c7bd9eca8b7e2011b86ec562c
User <diana09877@mail.com>
2024-05-14 18:01:37 +0600

    было реализована логика игрф камень ножницы бумага

--- telegram_bot_game/main.py
+++ telegram_bot_game/main.py
@@ -1,24 +1,31 @@
 import telebot
+import random
 from local_settings import API_TOKEN
-
-
 
 bot = telebot.TeleBot(API_TOKEN)
 
-
-# Handle '/start' and '/help'
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
     bot.reply_to(message, """\
-Hi there, I am EchoBot.
-I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
+Добро пожаловать в игру "камень, ножницы, бумага!" Выберите камень (к), ножницы (н), бумага(б)\
 """)
 
-
-# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
 @bot.message_handler(func=lambda message: True)
-def echo_message(message):
-    bot.reply_to(message, message.text)
-
+def play_game(message):
+    user_move = message.text.lower()
+    if user_move not in ['к', 'н', 'б']:
+        bot.reply_to(message, "Неправильный ввод. Пожалуйста, выберите камень (к), ножницы (н) или бумагу (б).")
+    else:
+        computer_move = random.choice(["к", "н", "б"])
+        if user_move == computer_move:
+            bot.reply_to(message, "Ничья!")
+        elif (user_move == 'к' and computer_move == 'н') or \
+             (user_move == 'н' and computer_move == 'б') or \
+             (user_move == 'б' and computer_move == 'к'):
+            bot.reply_to(message, "Вы выиграли!")
+        else:
+            bot.reply_to(message, "Компьютер победил!")
 
 bot.infinity_polling()
+
+
